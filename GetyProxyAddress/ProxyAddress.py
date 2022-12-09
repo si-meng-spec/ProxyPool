72f4b7628017d7eaf3ec4ffb243653fff717a235
祀梦 <3501646051@qq.com>
2022-12-10 00:59:01 +0800

    获取免费IP代理

---
+++ GetyProxyAddress/ProxyAddress.py
@@ -0,0 +1,34 @@
+from Logger.logger import Logger
+from Request import MyRequests
+
+
+class ProxyMetaclass(type):
+    def __new__(mcs, name, bases, attrs):
+        count = 0
+        attrs['__CrawlFunc__'] = []
+        for k, v in attrs.items():
+            if 'crawl_' in k:
+                attrs['__CrawlFunc__'].append(k)
+                count += 1
+        attrs['__CrawlFuncCount__'] = count
+        return type.__new__(mcs, name, bases, attrs)
+
+
+class ProxyAddressPool(object, metaclass=ProxyMetaclass):
+    def __init__(self):
+        self.request = MyRequests().sendRequestText
+        self.logger = Logger()
+
+    #   获取云代理网站上的公开代理
+    def crawl_ip3366(self, page_count=4):
+        for style in range(1, 3):
+            for url in [f"http://www.ip3366.net/free/?stype={style}&page={count}" for count in
+                        range(1, page_count + 1)]:
+                for inf in self.Template1(url):
+                    yield inf
+
+    #   获取89ip上的公开代理
+    def crawl_Pool89ip(self, page_count=8):
+        for url in [f"https://proxy.ip3366.net/free/?action=china&page={count}" for count in range(1, page_count + 1)]:
+            for inf in self.Template1(url):
+                yield inf
---
+++ GetyProxyAddress/__init__.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(__file__))
+
+from ProxyAddress import ProxyAddressPool
+
+
+__all__ = ['ProxyAddressPool']
