7cad6ba3badbbe39dce6caff46cae565bf47f854
祀梦 <3501646051@qq.com>
2022-12-12 00:08:48 +0800

    通过Verification的模块来验证当前代理是否可用

--- Storage/Storage.py
+++ Storage/Storage.py
@@ -1,31 +1,40 @@
 import json
 import random
-import os
+import os, sys
+
+sys.path.append(os.path.abspath(os.path.dirname(os.getcwd())))
+from Ver import Verification
 
 
 class IPProxyStorage:
     def __init__(self):
         self.__data = {"https": [], "http": []}
+        self.ver = Verification()
         self.folder_name = os.path.dirname(os.path.realpath(__file__))
 
-    def get_ip(self, accord: str = "http") -> str or None:
+    def get(self, accord: str = "http") -> str or None:
         accord = accord.lower()
         if accord in ['https', 'http'] and len(self.__data[accord]) != 0:
             return random.choice(self.__data[accord])
         return None
 
-    def add_ip(self, accord: str, ip_port: str) -> bool:
+    def add(self, accord: str, ip_port: str) -> bool:
         accord = accord.lower()
         if accord in ['https', 'http']:
             self.__data[accord].append(ip_port)
             return True
         return False
 
-    def save(self):
-        with open(f"{self.folder_name}\\data.json", 'w', encoding='utf-8') as f:
-            f.write(json.dumps(self.__data, indent=2))
+    def count(self):
+        return {'https': len(self.__data['https']), 'http': len(self.__data['http'])}
+
+    def all(self):
+        return self.__data[:]
+
+    def verip(self):
+        self.__data = self.ver.run(self.__data[:])
 
 
 if __name__ == "__main__":
     storage = IPProxyStorage()
-    storage.save()
+    storage.verip()
