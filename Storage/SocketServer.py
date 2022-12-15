4fa9b1fc9710159991dd312a87a88a232907f252
祀梦 <3501646051@qq.com>
2022-12-15 11:17:38 +0800

    添加SocketServer

---
+++ Storage/SocketServer.py
@@ -0,0 +1,30 @@
+import socket
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.dirname(os.getcwd())))
+from Logger import LogManager
+from Storage import IPProxyStorage
+
+
+class SocketServer:
+    def __init__(self, ip:str, port:int):
+        self.s = socket.socket()
+        self.s.bind((ip, port))
+        self.s.listen()
+        self.Log_manager = LogManager()
+        self.proxy = IPProxyStorage()
+        self.ip_port = f"{ip}:{port}"
+
+    def run(self):
+        while True:
+            client, address = self.s.accept()
+            # 只获取请求头的第一行,进行解析
+            data = client.recv(1024).decode('utf-8').split("\n")[0].replace("\r", "").split(" ")
+            if data[0] == 'GET' or data[0] == 'POST':
+                res = self.proxy.get(data[1][1:])
+                res = res if res is not None else ""
+                self.Log_manager.log("SocketServer",
+                                     f"客户机地址:{address[0]}:{address[1]},服务器地址:{self.ip_port},请求数据:{data[1][1:]},回应数据:{res}")
+                client.send(f'HTTP/1.1 200 OK\r\n\r\n{res}'.encode('utf-8'))
+                client.close()
--- Storage/__init__.py
+++ Storage/__init__.py
@@ -4,6 +4,7 @@ import sys
 sys.path.append(os.path.dirname(__file__))
 
 from IPStorage import IPProxyStorage
+from SocketServer import SocketServer
 from utils import singleton
 
-__all__ = ["IPProxyStorage", "singleton"]
+__all__ = ["IPProxyStorage", "singleton", 'SocketServer']
