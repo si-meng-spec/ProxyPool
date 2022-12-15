4b2008e85f586a9644a85e3dddaab7100ac71441
祀梦 <3501646051@qq.com>
2022-12-15 13:50:14 +0800

    添加开心代理,添加指定页面编码的功能

--- GetyProxyAddress/ProxyAddress.py
+++ GetyProxyAddress/ProxyAddress.py
@@ -18,12 +18,13 @@ class ProxyAddressPool(object, metaclass=ProxyMetaclass):
     def __init__(self):
         self.request = requests.get
 
-    def getHtml(self, url):
+    def getHtml(self, url, encoding='utf-8'):
         try:
-            res = self.request(url).text
+            res = self.request(url)
+            res.encoding = encoding
         except:
             return None
-        return res
+        return res.text
 
     def crawl_proxyip3366(self, page_count=4):
         for page in range(1, page_count + 1):
@@ -34,3 +35,21 @@ class ProxyAddressPool(object, metaclass=ProxyMetaclass):
                 _inf = [td.text() for td in tr.find("td").items()]
                 if len(_inf) > 0 and _inf[0] != "":
                     yield [_inf[3], f"{_inf[0]}:{_inf[1]}"]
+
+    def crawl_kxdaili(self, page_count=4):
+        for x in range(1, 3):
+            for y in range(1, page_count + 1):
+                html = self.getHtml(f"http://www.kxdaili.com/dailiip/{x}/{y}.html")
+                if html is None:
+                    continue
+                for tr in pq(html)("table.active tr").items():
+                    _inf = [td.text() for td in tr.find("td").items()]
+                    if len(_inf) > 0 and _inf[0] != "":
+                        for accord in _inf[3].split(","):
+                            yield [accord, f"{_inf[0]}:{_inf[1]}"]
+
+
+if __name__ == "__main__":
+    ProxyAddressPool = ProxyAddressPool()
+    for inf in ProxyAddressPool.crawl_kxdaili():
+        print(inf)
